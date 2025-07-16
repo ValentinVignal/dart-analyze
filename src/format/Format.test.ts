77dc2892e747cb4410b44344cdb1eada4ee0ef16
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-07-16 16:12:30 +0800

    Add more tests

--- src/format/Format.test.ts
+++ src/format/Format.test.ts
@@ -7,6 +7,9 @@ import { format } from './Format.js';
 import type { ActionOptionsSafe } from '../utils/ActionOptions.js';
 
 describe('Format', () => {
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
   it('should concatenate the cwd to files from the format command to compare them with the modified files', async () => {
     const actionOptions = {
       format: true,
@@ -73,5 +76,61 @@ describe('Format', () => {
     expect(ignoredFiles.has).toHaveBeenCalledWith('lib/file_0.dart');
     expect(ignoredFiles.has).toHaveBeenCalledWith('lib/file_1.dart');
     expect(ignoredFiles.has).toHaveBeenCalledWith('lib/file_2.dart');
+  });
+
+  it('should skip it if format is disabled', async () => {
+    const actionOptions = {
+      format: false,
+    } as ActionOptionsSafe;
+    const ignoredFiles: Partial<IgnoredFiles> = {};
+    const modifiedFiles: Partial<ModifiedFiles> = {};
+
+    const result = await format({
+      modifiedFiles: modifiedFiles as ModifiedFiles,
+      ignoredFiles: ignoredFiles as IgnoredFiles,
+      actionOptions: actionOptions,
+    });
+
+    expect(result.success).toEqual(true);
+    expect(result.count).toEqual(0); // It should succeed because format is disabled.
+  });
+
+  it('should use the provided format lines', async () => {
+    vi.spyOn(console, 'log').mockImplementation(() => {});
+    const actionOptions = {
+      format: true,
+      workingDirectory: 'cwd/actionOptionWorkingDirectory',
+      failOn: FailOnEnum.Format,
+      formatLines: [
+        'Changed lib/file_0.dart',
+        'Changed lib/file_1.dart',
+        'Changed lib/file_2.dart',
+      ],
+    } as ActionOptionsSafe;
+    const ignoredFiles: Partial<IgnoredFiles> = {
+      has: vi.fn(),
+    };
+    const modifiedFiles: Partial<ModifiedFiles> = {
+      has: vi.fn(),
+    };
+
+    (ignoredFiles.has as any).mockReturnValue(false);
+    (modifiedFiles.has as any).mockImplementation((file: string) => {
+      return [
+        'cwd/actionOptionWorkingDirectory/lib/file_0.dart',
+        'cwd/actionOptionWorkingDirectory/lib/file_1.dart',
+      ].includes(file);
+    });
+    const result = await format({
+      modifiedFiles: modifiedFiles as ModifiedFiles,
+      ignoredFiles: ignoredFiles as IgnoredFiles,
+      actionOptions: actionOptions,
+    });
+
+    expect(result.success).toEqual(false); // It should fail because some modified files are not formatted.
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    expect((result as any).files).toEqual(
+      new Set(['lib/file_0.dart', 'lib/file_1.dart']),
+    ); // It should return the unformatted files.
   });
 });
