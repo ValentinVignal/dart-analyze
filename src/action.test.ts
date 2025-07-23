cc299b9374597cecc6b65a7c3435da8d4e44ecc1
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-07-23 13:48:21 +0800

    Add more test

--- src/action.test.ts
+++ src/action.test.ts
@@ -277,4 +277,249 @@ describe('run', () => {
 - 1 formatting issue`,
     ]);
   });
+
+  it('should support when the analysis logs contain __w', async () => {
+    // Mock the environment variables representing the action inputs.
+    process.env.INPUT_TOKEN = 'token';
+    const workingDirectory = '/home/runner/work/repository/repository';
+    process.env.GITHUB_WORKSPACE = workingDirectory;
+
+    // Mock the Github API.
+    vi.mocked(github.context).eventName = 'pull_request';
+    vi.mocked(github.context).payload = {
+      pull_request: {
+        number: 123,
+        base: { sha: 'base-sha' },
+        head: { sha: 'head-sha' },
+      },
+    } as unknown as WebhookPayload;
+    vi.spyOn(github.context, 'repo', 'get').mockReturnValue({
+      owner: 'test-owner',
+      repo: 'test-repo',
+    });
+
+    const octokit: DeepPartial<InstanceType<typeof GitHub>> = {
+      rest: {
+        repos: {
+          compareCommitsWithBasehead: vi.fn().mockResolvedValue({
+            status: 200,
+            data: {
+              files: [
+                {
+                  filename: 'lib/file1.dart',
+                  patch: `
+@@ -1,4 +1,6 @@
+ import 'package:flutter/material.dart';
+
+-void main() => runApp(MyApp());
++void main() {
++  runApp(MyApp());
++}
+
+@@ -20,3 +20,5 @@
+ class MyApp extends StatelessWidget {
+-  Widget build(BuildContext context) => MaterialApp(home: Container());
++  Widget build(BuildContext context) {
++    return MaterialApp(home: Container());
++  }
+ }
+                  `,
+                },
+
+                {
+                  filename: 'lib/file2.dart',
+                  patch: `
+@@ -50,4 +50,6 @@
+ import 'package:flutter/material.dart';
+
+-void main() => runApp(MyApp());
++void main() {
++  runApp(MyApp());
++}
+
+@@ -200,3 +200,5 @@
+ class MyApp extends StatelessWidget {
+-  Widget build(BuildContext context) => MaterialApp(home: Container());
++  Widget build(BuildContext context) {
++    return MaterialApp(home: Container());
++  }
+ }
+                  `,
+                },
+              ],
+            },
+          }) as any,
+        },
+        issues: {
+          listComments: vi.fn().mockResolvedValue({
+            data: [],
+          }) as any,
+          createComment: vi.fn().mockResolvedValue({}) as any,
+        },
+      },
+    };
+
+    vi.mocked(github.getOctokit).mockReturnValue(
+      octokit as InstanceType<typeof GitHub>,
+    );
+
+    // Mock the read of the yaml file.
+    vi.mocked(path.join).mockImplementation((...args) => args.join('/'));
+    vi.mocked(path.resolve).mockReturnValueOnce(workingDirectory);
+    vi.mocked(path.resolve).mockReturnValueOnce(
+      'path/to/analysis_options.yaml',
+    );
+    vi.mocked(fs.existsSync).mockReturnValue(true);
+    vi.mocked(fs.readFileSync).mockReturnValue('');
+
+    // Mock the exec.exec function.
+    vi.mocked(exec.exec).mockImplementationOnce(
+      async (command, args, options) => {
+        const lines = [
+          // SEVERITY|TYPE|ERROR_CODE|FILE_PATH|LINE|COLUMN|LENGTH|ERROR_MESSAGE
+          'INFO|LINT|PREFER_CONST_CONSTRUCTORS|/path/to/file.dart|96|13|80|Prefer const with constant constructors.',
+          // # File 1.
+          // Info.
+          'INFO|LINT|ERROR_CODE_0|/__w/repository/repository/lib/file1.dart|1|1|1|Info message for file 1, hunk 1.', // Included.
+          'INFO|LINT|ERROR_CODE_1|/__w/repository/repository/lib/file1.dart|10|1|1|Ignored info message for file 1.',
+          'INFO|LINT|ERROR_CODE_2|/__w/repository/repository/lib/file1.dart|20|1|1|Info message for file 1, hunk 2.', // Included.
+          'INFO|LINT|ERROR_CODE_3|/__w/repository/repository/lib/file1.dart|50|1|1|Ignored info message for file 1.',
+          // Warning.
+          'WARNING|LINT|ERROR_CODE_4|/__w/repository/repository/lib/file1.dart|2|1|1|Warning message for file 1, hunk 1.', // Included.
+          'WARNING|LINT|ERROR_CODE_5|/__w/repository/repository/lib/file1.dart|11|1|1|Ignored warning message for file 1.',
+          'WARNING|LINT|ERROR_CODE_6|/__w/repository/repository/lib/file1.dart|21|1|1|Warning message for file 1, hunk 2.', // Included.
+          'WARNING|LINT|ERROR_CODE_7|/__w/repository/repository/lib/file1.dart|51|1|1|Ignored warning message for file 1.',
+          // Errors.
+          'ERROR|LINT|ERROR_CODE_8|/__w/repository/repository/lib/file1.dart|3|1|1|Error message for file 1, hunk 1.', // Included.
+          'ERROR|LINT|ERROR_CODE_9|/__w/repository/repository/lib/file1.dart|12|1|1|Ignored error message for file 1.',
+          'ERROR|LINT|ERROR_CODE_10|/__w/repository/repository/lib/file1.dart|22|1|1|Error message for file 1, hunk 2.', // Included.
+          'ERROR|LINT|ERROR_CODE_11|/__w/repository/repository/lib/file1.dart|52|1|1|Ignored error message for file 1.',
+
+          // # File 2.
+          // Info.
+          'INFO|LINT|ERROR_CODE_12|/__w/repository/repository/lib/file2.dart|1|1|1|Ignored info message for file 2.',
+          'INFO|LINT|ERROR_CODE_13|/__w/repository/repository/lib/file2.dart|50|1|1|Info message for file 2, hunk 1.', // Included.
+          'INFO|LINT|ERROR_CODE_14|/__w/repository/repository/lib/file2.dart|100|1|1|Ignored info message for file 2.',
+          'INFO|LINT|ERROR_CODE_15|/__w/repository/repository/lib/file2.dart|200|1|1|Info message for file 2, hunk 2.', // Included.
+          'INFO|LINT|ERROR_CODE_16|/__w/repository/repository/lib/file2.dart|500|1|1|Ignored info message for file 2.',
+          // Warning.
+          'WARNING|LINT|ERROR_CODE_17|/__w/repository/repository/lib/file2.dart|2|1|1|Ignored warning message for file 2.',
+          'WARNING|LINT|ERROR_CODE_18|/__w/repository/repository/lib/file2.dart|51|1|1|Warning message for file 2, hunk 1.', // Included.
+          'WARNING|LINT|ERROR_CODE_19|/__w/repository/repository/lib/file2.dart|101|1|1|Ignored warning message for file 2.',
+          'WARNING|LINT|ERROR_CODE_20|/__w/repository/repository/lib/file2.dart|201|1|1|Warning message for file 2, hunk 2.', // Included.
+          'WARNING|LINT|ERROR_CODE_21|/__w/repository/repository/lib/file2.dart|501|1|1|Ignored warning message for file 2.',
+          // Errors.
+          'ERROR|LINT|ERROR_CODE_22|/__w/repository/repository/lib/file2.dart|3|1|1|Ignored error message for file 2.',
+          'ERROR|LINT|ERROR_CODE_23|/__w/repository/repository/lib/file2.dart|52|1|1|Error message for file 2, hunk 1.', // Included.
+          'ERROR|LINT|ERROR_CODE_24|/__w/repository/repository/lib/file2.dart|102|1|1|Ignored error message for file 2.',
+          'ERROR|LINT|ERROR_CODE_25|/__w/repository/repository/lib/file2.dart|202|1|1|Error message for file 2, hunk 2.', // Included.
+          'ERROR|LINT|ERROR_CODE_26|/__w/repository/repository/lib/file2.dart|502|1|1|Ignored error message for file 2.',
+          // # File 3 (not modified).
+          'INFO|LINT|ERROR_CODE_27|/__w/repository/repository/lib/file3.dart|1|1|1|Info message for file 3',
+          'WARNING|LINT|ERROR_CODE_27|/__w/repository/repository/lib/file3.dart|2|1|1|Warning message for file 3',
+          'ERROR|LINT|ERROR_CODE_27|/__w/repository/repository/lib/file3.dart|3|1|1|Error message for file 3',
+        ];
+
+        for (const line of lines) {
+          options!.listeners!.stdout!(Buffer.from(line + '\n'));
+        }
+        return 0;
+      },
+    );
+    vi.mocked(exec.exec).mockImplementationOnce(
+      async (command, args, options) => {
+        const lines = [
+          'Changed lib/file1.dart',
+          'Changed lib/file3.dart',
+          'Formatted 10 files (2 changed) in 0.123s',
+        ];
+
+        for (const line of lines) {
+          options!.listeners!.stdout!(Buffer.from(line + '\n'));
+        }
+        return 0;
+      },
+    );
+
+    await run();
+
+    expect(
+      octokit.rest?.repos?.compareCommitsWithBasehead,
+    ).toHaveBeenCalledExactlyOnceWith({
+      basehead: 'base-sha...head-sha',
+      owner: 'test-owner',
+      repo: 'test-repo',
+    });
+    expect(exec.exec).toHaveBeenCalledWith(
+      'dart analyze --format machine',
+      [workingDirectory],
+      expect.anything(),
+    );
+    expect(exec.exec).toHaveBeenCalledWith(
+      'dart format',
+      ['-o', 'none', '.'],
+      expect.anything(),
+    );
+    expect(octokit.rest!.issues!.listComments).toHaveBeenCalledExactlyOnceWith({
+      owner: 'test-owner',
+      repo: 'test-repo',
+      issue_number: 123,
+    });
+    expect(octokit.rest!.issues!.createComment).toHaveBeenCalledExactlyOnceWith(
+      {
+        owner: 'test-owner',
+        repo: 'test-repo',
+        issue_number: 123,
+        body: `<!-- dart-analyze -->
+:x: Dart Analyzer found 13 issues
+- :x: **4 errors.**
+- :warning: **4 warnings.**
+- :eyes: 4 info logs.
+- :poop: 1 formatting issue
+---
+- :poop: :eyes: Info - \`lib/file1.dart\`:1:1 - Info message for file 1, hunk 1. (error_code_0). See [link](https://dart.dev/diagnostic/error_code_0) or [link](https://dart.dev/lints/error_code_0).
+- :poop: :eyes: Info - \`lib/file1.dart\`:20:1 - Info message for file 1, hunk 2. (error_code_2). See [link](https://dart.dev/diagnostic/error_code_2) or [link](https://dart.dev/lints/error_code_2).
+- :x: :warning: **Warning - \`lib/file1.dart\`:2:1 - Warning message for file 1, hunk 1. (error_code_4).** See [link](https://dart.dev/diagnostic/error_code_4) or [link](https://dart.dev/lints/error_code_4).
+- :x: :warning: **Warning - \`lib/file1.dart\`:21:1 - Warning message for file 1, hunk 2. (error_code_6).** See [link](https://dart.dev/diagnostic/error_code_6) or [link](https://dart.dev/lints/error_code_6).
+- :x: :bangbang: **Error - \`lib/file1.dart\`:3:1 - Error message for file 1, hunk 1. (error_code_8).** See [link](https://dart.dev/diagnostic/error_code_8) or [link](https://dart.dev/lints/error_code_8).
+- :x: :bangbang: **Error - \`lib/file1.dart\`:22:1 - Error message for file 1, hunk 2. (error_code_10).** See [link](https://dart.dev/diagnostic/error_code_10) or [link](https://dart.dev/lints/error_code_10).
+- :poop: :eyes: Info - \`lib/file2.dart\`:50:1 - Info message for file 2, hunk 1. (error_code_13). See [link](https://dart.dev/diagnostic/error_code_13) or [link](https://dart.dev/lints/error_code_13).
+- :poop: :eyes: Info - \`lib/file2.dart\`:200:1 - Info message for file 2, hunk 2. (error_code_15). See [link](https://dart.dev/diagnostic/error_code_15) or [link](https://dart.dev/lints/error_code_15).
+- :x: :warning: **Warning - \`lib/file2.dart\`:51:1 - Warning message for file 2, hunk 1. (error_code_18).** See [link](https://dart.dev/diagnostic/error_code_18) or [link](https://dart.dev/lints/error_code_18).
+- :x: :warning: **Warning - \`lib/file2.dart\`:201:1 - Warning message for file 2, hunk 2. (error_code_20).** See [link](https://dart.dev/diagnostic/error_code_20) or [link](https://dart.dev/lints/error_code_20).
+- :x: :bangbang: **Error - \`lib/file2.dart\`:52:1 - Error message for file 2, hunk 1. (error_code_23).** See [link](https://dart.dev/diagnostic/error_code_23) or [link](https://dart.dev/lints/error_code_23).
+- :x: :bangbang: **Error - \`lib/file2.dart\`:202:1 - Error message for file 2, hunk 2. (error_code_25).** See [link](https://dart.dev/diagnostic/error_code_25) or [link](https://dart.dev/lints/error_code_25).
+---
+- :poop:  \`lib/file1.dart\` is not formatted.`,
+      },
+    );
+    expect(vi.mocked(console.log).mock.calls).toEqual(
+      [
+        '::group:: Analyze dart code',
+        '::WARNING file=lib/file1.dart,line=1,col=1::Info message for file 1, hunk 1. (error_code_0) See https://dart.dev/diagnostic/error_code_0 or https://dart.dev/lints/error_code_0',
+        '::WARNING file=lib/file1.dart,line=20,col=1::Info message for file 1, hunk 2. (error_code_2) See https://dart.dev/diagnostic/error_code_2 or https://dart.dev/lints/error_code_2',
+        '::WARNING file=lib/file1.dart,line=2,col=1::Warning message for file 1, hunk 1. (error_code_4) See https://dart.dev/diagnostic/error_code_4 or https://dart.dev/lints/error_code_4',
+        '::WARNING file=lib/file1.dart,line=21,col=1::Warning message for file 1, hunk 2. (error_code_6) See https://dart.dev/diagnostic/error_code_6 or https://dart.dev/lints/error_code_6',
+        '::ERROR file=lib/file1.dart,line=3,col=1::Error message for file 1, hunk 1. (error_code_8) See https://dart.dev/diagnostic/error_code_8 or https://dart.dev/lints/error_code_8',
+        '::ERROR file=lib/file1.dart,line=22,col=1::Error message for file 1, hunk 2. (error_code_10) See https://dart.dev/diagnostic/error_code_10 or https://dart.dev/lints/error_code_10',
+        '::WARNING file=lib/file2.dart,line=50,col=1::Info message for file 2, hunk 1. (error_code_13) See https://dart.dev/diagnostic/error_code_13 or https://dart.dev/lints/error_code_13',
+        '::WARNING file=lib/file2.dart,line=200,col=1::Info message for file 2, hunk 2. (error_code_15) See https://dart.dev/diagnostic/error_code_15 or https://dart.dev/lints/error_code_15',
+        '::WARNING file=lib/file2.dart,line=51,col=1::Warning message for file 2, hunk 1. (error_code_18) See https://dart.dev/diagnostic/error_code_18 or https://dart.dev/lints/error_code_18',
+        '::WARNING file=lib/file2.dart,line=201,col=1::Warning message for file 2, hunk 2. (error_code_20) See https://dart.dev/diagnostic/error_code_20 or https://dart.dev/lints/error_code_20',
+        '::ERROR file=lib/file2.dart,line=52,col=1::Error message for file 2, hunk 1. (error_code_23) See https://dart.dev/diagnostic/error_code_23 or https://dart.dev/lints/error_code_23',
+        '::ERROR file=lib/file2.dart,line=202,col=1::Error message for file 2, hunk 2. (error_code_25) See https://dart.dev/diagnostic/error_code_25 or https://dart.dev/lints/error_code_25',
+        '::endgroup::',
+        '::group:: Analyze formatting',
+        '::warning file=lib/file1.dart:: lib/file1.dart is not formatted',
+        '::endgroup::',
+      ].map((s) => [s]),
+    );
+    expect(core.setFailed).not.toHaveBeenCalledWith([
+      `Dart Analyzer found 13 issues
+- 4 errors.
+- 4 warnings.
+- 4 info logs.
+- 1 formatting issue`,
+    ]);
+  });
 });
